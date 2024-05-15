f97fad1c8e26986af8155680c1875cc5796d6bc4
priirocha <prii.rocha@hotmail.com>
2024-05-15 12:08:33 +0100

    first commit

---
+++ frontend/src/pages/AddProductPage.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import AddProduct from "../components/AddProduct";
+                    
+const AddProductPage = () => {
+    return (
+        <>
+            <NavBar />
+            <AddProduct />
+        </>
+    );
+};
+                    
+export default AddProductPage;
---
+++ frontend/src/pages/UpdateProductPage.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import UpdateProduct from "../components/UpdateProduct";
+                    
+const UpdateProductPage = () => {
+    return (
+        <>
+            <NavBar />
+            <UpdateProduct />
+        </>
+    );
+};
+                    
+export default UpdateProductPage;
